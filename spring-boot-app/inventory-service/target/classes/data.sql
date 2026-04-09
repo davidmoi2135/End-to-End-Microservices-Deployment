68f42a87155283053f77e9d69d17694ef487ccb3
nguyentrinh2135 <caocaonguyentrinh210305@gmail.com>
2026-04-09 20:11:55 +0700

    Add Microservice-app

--- spring-boot-app/inventory-service/target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO inventory (sku_code, quantity) VALUES ('iPhone 15 Pro', 100);
-INSERT INTO inventory (sku_code, quantity) VALUES ('Samsung S24', 50);
-INSERT INTO inventory (sku_code, quantity) VALUES ('Macbook M3', 20);
--- spring-boot-app/inventory-service/target/classes/schema.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS inventory;
-CREATE TABLE inventory (
-    id SERIAL PRIMARY KEY,
-    sku_code VARCHAR(255) NOT NULL,
-    quantity INTEGER NOT NULL
-);
